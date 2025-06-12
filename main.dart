f58dfb532104d563b81a6c124ebfd308d0c5b53a
TDOV <130865591+TDOV@users.noreply.github.com>
2025-06-12 20:41:37 -0300

    trabalho 1

---
+++ main.dart
@@ -0,0 +1,66 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+
+  Widget build(BuildContext context) {
+    const title = 'Basic List';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(title)),
+        body: ListView(
+          children: const <Widget>[
+            ListTile(leading: Icon(Icons.map), title: Text('Undyne')),
+            ListTile(leading: Icon(Icons.photo_album), title: Text('Alphys')),
+            ListTile(leading: Icon(Icons.phone), title: Text('Papyrus')),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('A random idea:'),
+          Text(appState.current.asLowerCase),
+        ],
+      ),
+    );
+  }
+}
